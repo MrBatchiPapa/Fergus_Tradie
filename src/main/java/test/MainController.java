d1a1e842b79931ba15f6a2c84d1b69037d43c32b
MrBatchipapa <kevink314159@gmail.com>
2022-09-02 16:18:14 +1200

    Created
    feat: Created basic layout of app

---
+++ src/main/java/test/MainController.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+
+    
+}
